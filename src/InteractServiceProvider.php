037dc18e4f6506da3b079018e828c831d18397f8
Firose Hussain <github@firumon.com>
2019-03-16 16:54:18 +0530

    init

---
+++ src/InteractServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Milestone\Interact;
+
+use Illuminate\Support\ServiceProvider;
+
+class InteractServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
