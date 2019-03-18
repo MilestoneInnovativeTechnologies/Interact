6d4da5a5eca793b16958f1994aecb1ece5686f00
Firose Hussain <github@firumon.com>
2019-03-18 11:41:50 +0530

    Route prefix made to public, as can be configured from config, interact.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
+    $route_prefix = config('interact.route_prefix');
+    $prefix = trim($route_prefix,'\\/');
     Route::group([
         'namespace' => 'Milestone\\Interact',
-        'prefix' => 'interact'
+        'prefix' => $prefix,
     ],function(){
         Route::get('/',function(){ return '<form method="post" enctype="multipart/form-data"><input type="file" name="file"><input type="submit"></form>'; });
         Route::post('/','Controller@index');
