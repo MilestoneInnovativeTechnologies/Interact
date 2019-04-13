6d11c5225c5cd92a4937744b02a9ba2f99b6a1a9
Firose Hussain <github@firumon.com>
2019-04-13 17:20:37 +0530

    Caching used form table update monitoring. Implemented in service provider

---
+++ config/cache.stores.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Store
+    |--------------------------------------------------------------------------
+    |
+    | Define cache store for storing table update timing
+    |
+    */
+    'driver' => 'file',
+    'path' => storage_path('framework/cache/interact'),
+
+    ];
--- src/InteractServiceProvider.php
+++ src/InteractServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Milestone\Interact;
 
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class InteractServiceProvider extends ServiceProvider
@@ -16,6 +17,7 @@ class InteractServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom($this->project_root . 'config/interact.php', 'interact' );
+        $this->mergeConfigFrom($this->project_root . 'config/cache.stores.php', 'cache.stores.interact' );
     }
 
     /**
@@ -28,5 +30,8 @@ class InteractServiceProvider extends ServiceProvider
         $this->loadRoutesFrom($this->project_root . 'routes/web.php');
         $config = $this->project_root . 'config';
         $this->publishes([$config => config_path()]);
+
+        Event::listen(['eloquent.created: *'], function($name,$data) { cache()->store('interact')->put(Method::getModelClassFromEloquentName($name, 'eloquent.created: '),now()); });
+        Event::listen(['eloquent.updated: *'], function($name,$data) { cache()->store('interact')->put(Method::getModelClassFromEloquentName($name,'eloquent.updated: '),now()); });
     }
 }
