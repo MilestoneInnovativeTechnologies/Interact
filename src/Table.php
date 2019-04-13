d2c5c895ffc1508c7b2283e90735f896afcc001d
Firose Hussain <github@firumon.com>
2019-04-13 17:21:49 +0530

    Distribute interface updated by adding export methods

--- src/Table.php
+++ src/Table.php
@@ -22,7 +22,25 @@ interface Table
      * @return array
      */
     public function getImportAttributes();
+    /**
+     * A 2-D array which maps each element in the export attributes to
+     * the method in same class file which return the value from each eloquent record
+     * or the key in eloquent record which is to be the value for that particular attribute
+     * If no key found for an attribute in map, the attribute will be searched
+     * in record array, if found corresponding value will be returned
+     * or return null
+     *
+     *
+     * @return array
+     */
+    public function getExportMappings();
 
+    /**
+     * Array with all the attributes to be filled in this interaction
+     *
+     * @return array
+     */
+    public function getExportAttributes();
     /**
      * This function must return the primary id of the data row from the table is interacting
      * This function may either look into any model to get primary id value or generate from the data row
