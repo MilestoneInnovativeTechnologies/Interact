fcaa0aee33bfe115b5b5cc0a2c766bd8db68cb8e
Firose Hussain <github@firumon.com>
2019-05-06 12:00:26 +0530

    Sync controller updated

--- src/SyncController.php
+++ src/SyncController.php
@@ -3,11 +3,150 @@
 namespace Milestone\Interact;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Arr;
 
 class SyncController extends Controller
 {
+    private $requestTable, $client, $sync, $underlyingTable;
+    private $exportInteractObjectAttributes, $exportInteractObjectMappings;
+    private $importInteractObjectAttributes, $importInteractObjectMappings;
+    private $exportNewRecordsQuery, $exportUpdatedRecordsQuery;
+    private $primaryKeys;
+
+
     public function index($client,$table){
-        return SYNC::client($client,$table)->get();
+        $this->setupSync($table,$client); $this->setExportInteractObjectProperties();
+        $Activities = $this->getExportNewRecords();
+        $this->startImportNewRecords();
+        if($Activities && !empty($Activities)) return Out::data($Activities);
     }
     public function delete(Request $request){ if($request->has('client')); SYNC::delete($request->client); }
+
+    public function setupSync($table,$client){
+        parent::initSync($table);
+        $this->requestTable = $table; $this->client = $client;
+        $this->sync = SYNC::client($client,$table)->get();
+        $this->underlyingTable = $this->model->getTable();
+    }
+
+    private function setExportInteractObjectProperties(){
+        $this->exportInteractObjectAttributes = $this->getCallMethod($this->object,SYNCHelper::$method_get_attributes);
+        $this->exportInteractObjectMappings = $this->getCallMethod($this->object,SYNCHelper::$method_get_mappings);
+        $table = $this->requestTable;
+        $created_at = $this->getTimesFromSync('created')['client']->toDateTimeString();
+        $updated_at = $this->getTimesFromSync('updated')['client']->toDateTimeString();
+        $array = compact('table','created_at','updated_at');
+        foreach($array as $key => $value) if(property_exists($this->object,$key)) $this->object->$key = $value;
+    }
+
+    private function getExportNewRecords(){
+        $Activities = [];
+        if($this->checkIfHaveNewRecord('updated')){
+            $activity = $this->getNewlyUpdatedActivity($this->requestTable,$this->getTimesFromSync('created')['client']->toDateTimeString(),$this->getTimesFromSync('updated')['client']->toDateTimeString());
+            if($activity) $Activities[] = $activity;
+        }
+        if($this->checkIfHaveNewRecord('created')){
+            $activity = $this->getNewlyCreatedActivity($this->requestTable,$this->getTimesFromSync('created')['client']->toDateTimeString());
+            if($activity) $Activities[] = $activity;
+        }
+        return $Activities;
+    }
+
+    private function checkIfHaveNewRecord($type){
+        if($type === 'updated' && !Arr::get($this->sync,'table.updated')) return false;
+        if($type === 'created' && !Arr::get($this->sync,'table.created')) return true;
+        $table = Carbon::now(); $client = Carbon::create(1900);
+        extract($this->getTimesFromSync($type));
+        return $table->greaterThan($client);
+    }
+    private function getTimesFromSync($type){
+        $table_sync_time = Arr::get($this->sync,"table.{$type}",0);
+        $client_sync_time = Arr::get($this->sync,"client.{$type}",0);
+        $table = $table_sync_time ? Carbon::createFromTimeString($table_sync_time) : Carbon::now();
+        $client = $client_sync_time ? Carbon::createFromTimeString($client_sync_time) : Carbon::create(1900);
+        return compact('table','client');
+    }
+
+    private function getNewlyCreatedActivity($table,$created_at){
+        $data = $this->fetchExportNewlyCreatedRecords($created_at);
+        if($data->isEmpty()) return null;
+        $activity = SYNCHelper::wrapWithActivityProperties($table,'pk','create',$data);
+        $this->updateCreatedActivityTimes($activity,$created_at); return $activity;
+    }
+    private function getNewlyUpdatedActivity($table,$created_at,$updated_at){
+        $data = $this->fetchExportNewlyUpdatedRecords($created_at,$updated_at);
+        if($data->isEmpty()) return null;
+        $activity = SYNCHelper::wrapWithActivityProperties($table,'pk','update',$data);
+        $this->updateUpdatedActivityTimes($activity,$updated_at); return $activity;
+    }
+
+    private function setExportObjectProperties($table,$created_at,$updated_at){
+        $this->exportInteractObjectAttributes = $this->getCallMethod($this->object,SYNCHelper::$method_get_attributes);
+        $this->exportInteractObjectMappings = $this->getCallMethod($this->object,SYNCHelper::$method_get_mappings);
+        $array = compact('table','created_at','updated_at');
+        foreach($array as $key => $value) if(property_exists($this->object,$key)) $this->object->$key = $value;
+    }
+    private function fetchExportNewlyCreatedRecords($created_at){
+        $exportNewRecordsQuery = SYNCHelper::newlyCreatedRecordsFetchQuery($this->model,$created_at);
+        $this->exportNewRecordsQuery = $this->getCallMethod($this->object,SYNCHelper::$pre_export_get,[$exportNewRecordsQuery]) ?: $exportNewRecordsQuery;
+        return $this->exportNewRecordsQuery->get();
+    }
+    private function fetchExportNewlyUpdatedRecords($created_at,$updated_at){
+        $exportUpdatedRecordsQuery = SYNCHelper::newlyUpdatedRecordsFetchQuery($this->model,$created_at,$updated_at);
+        $this->exportUpdatedRecordsQuery = $this->getCallMethod($this->object,SYNCHelper::$pre_export_update,[$exportUpdatedRecordsQuery]) ?: $exportUpdatedRecordsQuery;
+        return $this->exportUpdatedRecordsQuery->get();
+    }
+    private function updateCreatedActivityTimes(&$activity,$query_created){
+        SYNC::client($this->client,$this->underlyingTable)->setCreated(now()->toDateTimeString(),Arr::get($activity,'record.created_at'));
+        if(!Arr::get($this->sync,'client.updated'))
+            SYNC::client($this->client,$this->underlyingTable)->setUpdated(now()->toDateTimeString(),Arr::get($activity,'record.created_at'));
+        Arr::set($activity,'query.created_at',$query_created);
+    }
+    private function updateUpdatedActivityTimes(&$activity,$query_updated){
+        SYNC::client($this->client,$this->underlyingTable)->setUpdated(now()->toDateTimeString(),Arr::get($activity,'record.updated_at'));
+        Arr::set($activity,'query.updated_at',$query_updated);
+    }
+
+
+
+    private function startImportNewRecords(){
+        $Activities = $this->getUploadedFileContent(); if(!$Activities || empty($Activities)) return;
+        $this->model->unguard(); foreach($Activities as $activity){
+            $this->primaryKeys = $activity['primary_key'];
+            $this->setImportInteractObjectProperties($activity); $this->getCallMethod($this->object,SYNCHelper::$pre_import,[$activity]);
+            if(!empty($activity['data'])) foreach($activity['data'] as $record){
+                if($this->doNeedToSkipThisImport($record)) continue;
+                $id = call_user_func_array([$this->object,SYNCHelper::$method_get_primary_id],[$record]);
+                if($id) $this->doUpdateImportRecord($id,$record);
+                else $this->doInsertImportRecord($record);
+            }
+            $this->getCallMethod($this->object,SYNCHelper::$post_import,[$activity,[]]);
+        }
+
+    }
+    private function setImportInteractObjectProperties($content){
+        $this->importInteractObjectAttributes = $this->getCallMethod($this->object,SYNCHelper::$method_import_attributes);
+        $this->importInteractObjectMappings = $this->getCallMethod($this->object,SYNCHelper::$method_import_mappings);
+        foreach($content as $Key => $Value) if(property_exists($this->object,$Key)) $this->object->$Key = $Value;
+    }
+    private function doNeedToSkipThisImport($record){
+        if(method_exists($this->object,SYNCHelper::$method_get_is_valid)){
+            return !call_user_func([$this->object,SYNCHelper::$method_get_is_valid],$record);
+        } return false;
+    }
+    private function updateInteractObjectMode($mode){ if(property_exists($this->object,'mode')) $this->object->mode = $mode; }
+    private function doInsertImportRecord($record){
+        $this->updateInteractObjectMode('create');
+        $newModel = $this->model->create($this->getFilledAttributes($this->importInteractObjectAttributes,$this->importInteractObjectMappings,$record));
+        SYNC::client($this->client,$this->underlyingTable)->setCreated(now()->toDateTimeString(),$newModel->created_at->toDateTimeString());
+        $this->getCallMethod($this->object,SYNCHelper::$method_imported,[$record,$newModel->getKey()]);
+    }
+    private function doUpdateImportRecord($ID,$record){
+        $this->updateInteractObjectMode('update');
+        $selectedModel = $this->model->find($ID);
+        $selectedModel->forceFill($this->getFilledAttributes($this->importInteractObjectAttributes,$this->importInteractObjectMappings,$record))->save();
+        SYNC::client($this->client,$this->underlyingTable)->setUpdated(now()->toDateTimeString(),$selectedModel->updated_at->toDateTimeString());
+        $this->getCallMethod($this->object,SYNCHelper::$method_imported,[$record,$selectedModel->getKey()]);
+    }
 }
