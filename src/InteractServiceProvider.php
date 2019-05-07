2ee641c19cda83b654c37e533b18ef80609a266c
Firose Hussain <github@firumon.com>
2019-05-07 17:21:24 +0530

    No extra configs, removed codes

--- src/InteractServiceProvider.php
+++ src/InteractServiceProvider.php
@@ -7,10 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class InteractServiceProvider extends ServiceProvider
 {
     protected $project_root = __DIR__ . '/..';
-    protected $bindConfigs = [
-        'cache.stores' => 'cache_stores',
-        'filesystems.disks' => 'filesystems_disks'
-    ];
 
     /**
      * Register services.
@@ -20,7 +16,6 @@ class InteractServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom($this->pathConfig('interact.php'), 'interact' );
-        $this->mergeConfigExtras();
     }
 
     /**
@@ -37,14 +32,4 @@ class InteractServiceProvider extends ServiceProvider
 
     private function pathConfig($file = null){ return $this->path('config',$file); }
     private function path($folder = null,$file = null){ return trim(implode('/',[$this->project_root,$folder,$file]),'\\\/'); }
-
-    private function mergeConfigExtras(){
-        foreach ($this->bindConfigs as $key => $interact_key)
-            $this->mergeConfig($key,config('interact.' . $interact_key));
-    }
-
-    private function mergeConfig($key,$content){
-        $config = $this->app['config']->get($key, []);
-        $this->app['config']->set($key, array_merge($content, $config));
-    }
 }
