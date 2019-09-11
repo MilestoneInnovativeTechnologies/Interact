c4db54ce3386b7d7916d3b70609a240771af7884
Firose <firumon@github.com>
2019-09-11 10:39:19 +0530

    Underlying table - caching will be done from inside class

--- src/UnderlyingTableController.php
+++ src/UnderlyingTableController.php
@@ -2,10 +2,18 @@
 
 namespace Milestone\Interact;
 
+use Illuminate\Support\Facades\Cache;
+
 class UnderlyingTableController extends Controller
 {
+
+    public static $cache_store = 'uTable';
+
     public static function table($table){
-        $Cls = new self(); $Cls->initSync($table);
-        return $Cls->model->getTable();
+        $underlyingTable = Cache::store(self::$cache_store)->rememberForever($table,function() use($table){
+            $Cls = new self(); $Cls->initSync($table);
+            return $Cls->model->getTable();
+        });
+        return $underlyingTable;
     }
 }
